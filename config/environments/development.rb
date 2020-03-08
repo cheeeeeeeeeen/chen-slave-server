873dfd19fabf42cd5d7a998444399c425dc92460
Cheeeeeeeeeen <blancfaye7@gmail.com>
2020-03-09 01:11:25 +0800

    More configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on the local file system
+  # (see config/storage.yml for options)
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
@@ -44,7 +45,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
